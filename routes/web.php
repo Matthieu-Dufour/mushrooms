f8dc430d66097b506c21953b6a9b6f7bf3db701e
Matthieu Dufour <matthieu.dufour3@etu.univ-lorraine.fr>
2019-02-16 14:44:05 +0100

    Inscription

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inscription', function () {
+    return view('inscription');
+});
+
+Route::post('/inscription', function () {
+
+    request()->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required', 'confirmed', 'min:8'],
+        'password_confirmation' => ['required'],
+    ], [
+        'password.min' => 'Pour des raisons de sécurité, votre mot de passe doit faire :min caractères minimum.'
+    ]);
+
+    $user = App\Users::create([
+        'email' => request ('email'),
+        'password' => bcrypt(request('password')),
+    ]);
+
+    return "yessaille, mail : " . request('email') . ", mdp : " . request('password');
+});
