0a875ff25722b469827637550291df387cd698ef
Matthieu Dufour <matthieu.dufour3@etu.univ-lorraine.fr>
2019-03-29 10:28:24 +0100

    seeder confusions

--- database/seeds/ConfusionsTableSeeder.php
+++ database/seeds/ConfusionsTableSeeder.php
@@ -14,11 +14,67 @@ class ConfusionsTableSeeder extends Seeder
     {
         DB::table('confusions')->insert([
             'mushroom1_id' => '1',
-            'mushroom2_id' => '2',
+            'mushroom2_id' => '3',
         ]);
         DB::table('confusions')->insert([
-            'mushroom1_id' => '2',
+            'mushroom1_id' => '3',
             'mushroom2_id' => '1',
+        ]);
+
+
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '4',
+            'mushroom2_id' => '5',
+        ]);
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '5',
+            'mushroom2_id' => '4',
+        ]);
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '4',
+            'mushroom2_id' => '1',
+        ]);
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '1',
+            'mushroom2_id' => '4',
+        ]);
+
+
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '5',
+            'mushroom2_id' => '1',
+        ]);
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '1',
+            'mushroom2_id' => '5',
+        ]);
+        
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '6',
+            'mushroom2_id' => '7',
+        ]);
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '6',
+            'mushroom2_id' => '1',
+        ]);
+        
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '7',
+            'mushroom2_id' => '6',
+        ]);
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '1',
+            'mushroom2_id' => '6',
+        ]);
+
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '7',
+            'mushroom2_id' => '8',
+        ]);
+
+        DB::table('confusions')->insert([
+            'mushroom1_id' => '8',
+            'mushroom2_id' => '7',
         ]);
     }
 }
